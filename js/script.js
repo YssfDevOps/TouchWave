cb2a764c8a8d8c19414a662568da08eb0e452774
YssfDevOps <youssef.cah.7912@gmail.com>
2023-11-08 19:46:39 +0100

    All done

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function selectMarca(element) {
+    // Comprueba si la marca ya está seleccionada
+    if (element.classList.contains('selected')) {
+        // Si la marca ya está seleccionada, deselecciónala y borra los productos
+        element.classList.remove('selected');
+        document.getElementById('productos').innerHTML = '';
+    } else {
+        // Si la marca no está seleccionada, deselecciona todas las marcas
+        document.querySelectorAll('.marca').forEach(function(marca) {
+            marca.classList.remove('selected');
+        });
+
+        // Selecciona la marca y carga los productos
+        element.classList.add('selected');
+        loadProductos(element.id);
+    }
+}
